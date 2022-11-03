e08b336d62912a5dd8ebb7538563cfef705b9aeb
Ramiro Coronel <Ramirocoronelcardenas@gmail.com>
2022-11-03 12:23:24 -0300

    Se realizo la funcionalidad de los botones de la card (mostrar, ocultar)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
+let btnVerMas = document.getElementById(`btnVerMas`);
+btnVerMas.addEventListener(`click` , verMas);
 
 function cambiarTitulo(){
-    console.log(`cambiar titulo`);
     let titulo = document.querySelector(`h2`);
-    console.log(titulo);
     titulo.innerHTML = `ARIES. Del 21/03 al 20/04`
     titulo.className = `display-5 tituloSigno text-light`
+
+}
+
+function verMas(){
+    console.log(`desde la funcion ver mas`);
+    let articulePadre = document.getElementById(`noticia`);
+    if(btnVerMas.innerHTML === `Ver Mas`){
+    let parrafoNuevo = document.createElement(`p`)
+    parrafoNuevo.innerHTML = `Aries está representado por el carnero. Es un signo del elemento Fuego asociado a la primera carta astrológica, el primero signo del zodíaco. Su regente es el planeta Marte. Forman parte del signo de Aries las personas nacidas entre el 21 de marzo y el 20 de abril.`
+    parrafoNuevo.className = `lead, text-light`
+    articulePadre.appendChild(parrafoNuevo)
+    btnVerMas.innerHTML = `Ver menos`;
+    btnVerMas.className = `btn btn-outline-primary`;
+
+    }else{
+       
+        articulePadre.removeChild(articulePadre.children[1])
+        btnVerMas.innerHTML = "Ver Mas";
+        btnVerMas.className = "btn btn-outline-primary";
+
+    }
+
+
+
 }
