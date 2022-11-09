36c2631ca2a0962d624ba10c5d7529983cbb75b2
Ramiro Coronel <Ramirocoronelcardenas@gmail.com>
2022-11-09 16:31:25 -0300

    Se cambio el maquetado y se empezo de 0 la parte de funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 let btnVerMas = document.getElementById(`btnVerMas`);
 btnVerMas.addEventListener(`click` , verMas);
 
+let btnEnviar = document.getElementById(`btnEnviar`)
+btnEnviar.addEventListener(`click`, enviarFecha);
+
+let fechaIngresada = document.getElementById
+
+parrafoNuevo.innerHTML =`Representado por el toro y regido por las tendencias de Venus, es el segundo de la rueda zodiacal y primero del elemento Tierra. Pertenecen al signo de Tauro quienes nacieron entre el 21 de abril y el 21 de mayo.`
+
+function enviarFecha(){}
+
 function cambiarTitulo(){
     let titulo = document.querySelector(`h2`);
     titulo.innerHTML = `ARIES. Del 21/03 al 20/04`
@@ -9,8 +18,8 @@ function cambiarTitulo(){
 }
 
 function verMas(){
-    console.log(`desde la funcion ver mas`);
-    let articulePadre = document.getElementById(`noticia`);
+   
+    let articulePadre = document.getElementById(`Signos`);
     if(btnVerMas.innerHTML === `Ver Mas`){
     let parrafoNuevo = document.createElement(`p`)
     parrafoNuevo.innerHTML = `Aries está representado por el carnero. Es un signo del elemento Fuego asociado a la primera carta astrológica, el primero signo del zodíaco. Su regente es el planeta Marte. Forman parte del signo de Aries las personas nacidas entre el 21 de marzo y el 20 de abril.`
@@ -29,4 +38,10 @@ function verMas(){
 
 
 
+}
+function describir(){
+let articulePadre = document.getElementById(`Signos`);
+if(btnVerMas.innerHTML === `Ver Mas`){
+    let parrafoNuevo = document.createElement(`p`)
+}
 }
